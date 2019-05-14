158b7323031fa83b53060c27e2f5799662df5380
meinteil666 <sebastiandejesusmoretottito@gmail.com>
2019-05-14 11:48:47 -0500

    asds

---
+++ hola.java
@@ -0,0 +1,7 @@
+
+
+public class Hola{
+	public static void main(String[]args){
+		System.out.print("¡BIENVENIDO!!! \n Ahora estamos en el Módulo IV. \n Prometo esforzarme día a día, mucho más que el módulo \n anterior");
+} 
+}
